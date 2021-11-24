a065c0758a64ac61ec6cdaee9ca60802f6b10980
Hugo Gouveia <hgchrome@gmail.com>
2021-11-24 20:03:03 -0300

    refactor: Add tests and types to CPF.isValid

--- app/src/CPF.ts
+++ app/src/CPF.ts
@@ -1,46 +1,48 @@
 export default class CPF {
-    public static validate(cpf: string) {
+    public static isValid(cpf: string): boolean {
         const strippedCpf: string = CPF.stripCPF(cpf);
         if (strippedCpf.length !== 11) return false;
         if (CPF.hasAnyNaNCharacter(strippedCpf)) return false;
         if (CPF.areAllDigitsTheSame(strippedCpf)) return false;
-        const expectedVerifyingDigits = CPF.getVerifyingDigits(strippedCpf);
-        const informedVerifyingDigits = strippedCpf.substring(strippedCpf.length - 2, strippedCpf.length);
-        return informedVerifyingDigits == expectedVerifyingDigits;
-    }
-
-    private static stripCPF(str: string): string {
-        return str.replace(RegExp('\\.|\\-| ', 'gi'), '');
-    }
-
-    private static areAllDigitsTheSame(str: string) {
-        return str.split('').every(c => c === str[0]);
-    }
-
-    private static hasAnyNaNCharacter(str: string) {
-        return !str.split('').every(c => !isNaN(parseInt(c)));
+        const expectedVerifyingDigits: string = CPF.calculateVerifyingDigits(strippedCpf);
+        const informedVerifyingDigits: string = CPF.getVerifyingDigits(strippedCpf);
+        return informedVerifyingDigits === expectedVerifyingDigits;
     }
 
     private static getVerifyingDigits(strippedCpf: string): string {
+        return strippedCpf.substring(strippedCpf.length - 2, strippedCpf.length);
+    }
+
+    private static stripCPF(str: string): string {
+        const regexToMatchFormattingSymbols = '\\.|\\-| ';
+        return str.replace(RegExp(regexToMatchFormattingSymbols, 'g'), '');
+    }
+
+    private static areAllDigitsTheSame(str: string): boolean {
+        return str.split('').every(c => c === str[0]);
+    }
+
+    private static hasAnyNaNCharacter(str: string): boolean {
+        return !str.split('').every(c => !isNaN(parseInt(c)));
+    }
+
+    private static calculateVerifyingDigits(strippedCpf: string): string {
         const firstVerifyingDigit = CPF.getVerifyingDigit(strippedCpf.substr(0, 9));
-        const secondVerifyingDigit = CPF.getVerifyingDigit(strippedCpf.substr(0, 9) + firstVerifyingDigit);
+        const cpfWithFirstVerifyingDigit = strippedCpf.substr(0, 9) + firstVerifyingDigit;
+        const secondVerifyingDigit = CPF.getVerifyingDigit(cpfWithFirstVerifyingDigit);
         return '' + firstVerifyingDigit + '' + secondVerifyingDigit;
     }
 
-    private static getVerifyingDigit(str: string) {
-        const sumForVerifyingDigit: number = CPF.getVerifyingSum(str);
-        const rest = (sumForVerifyingDigit % 11);
+    private static getVerifyingDigit(str: string): string {
+        const rest = (CPF.getVerifyingSum(str) % 11);
         const verifyingDigit = (rest < 2) ? 0 : 11 - rest;
-        return verifyingDigit;
+        return verifyingDigit.toString();
     }
 
     private static getVerifyingSum(str: string): number {
-        let verifyingSum: number = 0;
         const maxFactor = str.length + 1;
-        for (let nCount = maxFactor; nCount >= 2; nCount--) {
-            const currentDigit: number = parseInt(str[maxFactor - nCount]);
-            verifyingSum = verifyingSum + (nCount) * currentDigit;
-        };
-        return verifyingSum;
+        return str.split('')
+            .reduce((peviousDigit, currentDigit, index) =>
+                (peviousDigit + (parseInt(currentDigit) * (maxFactor - index))), 0);
     }
 }
--- app/test/CPF.test.ts
+++ app/test/CPF.test.ts
@@ -2,42 +2,60 @@ import CPF from '../src/CPF'
 
 test("Should return false if string is empty", function () {
     const badCPF: string = '';
-    const isValid = CPF.validate(badCPF);
+    const isValid = CPF.isValid(badCPF);
     expect(isValid).toBe(false);
 });
 
 test("Should return false if all digits are the same", function () {
     const badCPF: string = '111.111.111-11';
-    const isValid = CPF.validate(badCPF);
+    const isValid = CPF.isValid(badCPF);
+    expect(isValid).toBe(false);
+});
+
+test("Should return false if has any letters", function () {
+    const badCPF: string = 'a935.411.347-80';
+    const isValid = CPF.isValid(badCPF);
     expect(isValid).toBe(false);
 });
 
 test("Should return false if has any char that is not a digit", function () {
-    const badCPF: string = '1)1.111.111-11';
-    const isValid = CPF.validate(badCPF);
+    const badCPF: string = '9)5.411.347-80';
+    const isValid = CPF.isValid(badCPF);
     expect(isValid).toBe(false);
 });
 
 test("Should return false if has more than 11 digits", function () {
     const badCPF: string = '0111.111.111-11';
-    const isValid = CPF.validate(badCPF);
+    const isValid = CPF.isValid(badCPF);
     expect(isValid).toBe(false);
 });
 
 test("Should return false if has less than 11 digits", function () {
     const badCPF: string = '1111111111';    
-    const isValid = CPF.validate(badCPF);
+    const isValid = CPF.isValid(badCPF);
     expect(isValid).toBe(false);
 });
 
 test("Should return false if verifying digits are wrong", function () {
     const badCPF: string = '123.456.789-99';
-    const isValid = CPF.validate(badCPF);
+    const isValid = CPF.isValid(badCPF);
     expect(isValid).toBe(false);
 });
 
 test("Should return true if verifying digits are correct", function () {
     const goodCPF: string = '935.411.347-80';
-    const isValid = CPF.validate(goodCPF);
+    const isValid = CPF.isValid(goodCPF);
+    expect(isValid).toBe(true);
+});
+
+test("Should return true if verifying digits are correct even with spaces in string", function () {
+    const goodCPF: string = '9   35   .4   11.347      -80';
+    const isValid = CPF.isValid(goodCPF);
+    expect(isValid).toBe(true);
+});
+
+test("Should return true if verifying digits even without formatting", function () {
+    const goodCPF: string = '93541134780';
+    const isValid = CPF.isValid(goodCPF);
     expect(isValid).toBe(true);
 });
