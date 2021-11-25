0523ba74d49b691b260ba73776dc0830bbb00efd
Hugo Gouveia <hgchrome@gmail.com>
2021-11-25 00:00:18 -0300

    feat: Add item list to Order
    Add items and:
    - Calculate total number of items
    - Calculate total price of order
    
    Closes #5

--- app/src/Order.ts
+++ app/src/Order.ts
@@ -1,8 +1,30 @@
 import CPF from '../src/CPF';
+import Item from './Item';
 
 export default class Order {
     CPF: CPF;
+    items: Array<{
+        item: Item,
+        quantity: number
+    }>;
+    
     constructor(cpf: string){
         this.CPF = new CPF(cpf);
+        this.items = new Array;
+    }
+
+    addItem(item: any, quantity: number): void {
+        this.items.push({item, quantity});
+    }
+
+    getItemCount(): number {
+        return this.items.length;
+    }
+    
+    getTotal(): number {
+        return this.items.reduce(
+            (previous, current, _) => (previous + current.item.getPrice()*current.quantity),
+            0
+        )
     }
 }
--- app/test/Order.test.ts
+++ app/test/Order.test.ts
@@ -1,12 +1,48 @@
+import Item from '../src/Item';
 import Order from '../src/Order'
 
-describe.only('Creating Order', () => {
+describe('Creating Order', () => {
     test('Should not create an Order with invalid CPF', () => {
         expect(() => new Order('111.111.111-11')).toThrow(new Error('Invalid CPF'));
     });
-    
+
     test('Should create an Order with valid CPF', () => {
         const order = new Order('935.411.347-80');
         expect(order).toBeDefined();
+    });
+
+    describe('Adding items', () => {
+        test('Should create an Order with 1 item', () => {
+            const order = new Order('935.411.347-80');
+            order.addItem(new Item(1, 'Books', 'Clean Architecture', 50), 1);
+            const itemCount = order.getItemCount();
+            expect(itemCount).toBe(1);
+        });
+
+        test('Should create an Order with 3 items', () => {
+            const order = new Order('935.411.347-80');
+            order.addItem(new Item(1, 'Books', 'Clean Architecture', 50), 1);
+            order.addItem(new Item(2, 'Books', 'Clean Code', 70), 1);
+            order.addItem(new Item(3, 'Food', 'Coffee Beans 1kg', 10), 4);
+            const itemCount = order.getItemCount();
+            expect(itemCount).toBe(3);
+        });
+
+        test('Should return the order total', () => {
+            const order = new Order('935.411.347-80');
+            order.addItem(new Item(1, 'Books', 'Clean Architecture', 50), 1);
+            order.addItem(new Item(2, 'Books', 'Clean Code', 70), 1);
+            const total = order.getTotal();
+            expect(total).toBe((50 * 1 + 70 * 1));
+        });
+
+        test('Should return the order total with items having different quantities', () => {
+            const order = new Order('935.411.347-80');
+            order.addItem(new Item(1, 'Books', 'Clean Architecture', 50), 1);
+            order.addItem(new Item(2, 'Books', 'Clean Code', 70), 1);
+            order.addItem(new Item(3, 'Food', 'Coffee Beans 1kg', 10), 4);
+            const total = order.getTotal();
+            expect(total).toBe((50 * 1 + 70 * 1 + 10 * 4));
+        });
     });
 });
