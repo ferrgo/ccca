95d62090b61f1234a01f12b70528f61d1f6a8e1b
Hugo Gouveia <hgchrome@gmail.com>
2021-11-26 18:13:39 -0300

    test: Refactor tests for a reusable approach
    Refactor tests and try jest-sonar-reporter for CI

--- app/test/Order.test.ts
+++ app/test/Order.test.ts
@@ -1,6 +1,6 @@
 import Coupon from '../src/Coupon';
 import Item from '../src/Item';
-import Order from '../src/Order'
+import Order from '../src/Order';
 
 describe('Creating Order', () => {
     test('Should not create an Order with invalid CPF', () => {
@@ -48,50 +48,24 @@ describe('Creating Order', () => {
     });
 
     describe('Applying coupons', () => {
-        test('Should apply discount of 10% over total with coupon', () => {
+        const totalPrice = (50*1+70*1+10*4);
+        test.each([
+            { coupon: 10, discountedTotal: (0.9)*totalPrice },
+            { coupon: 15, discountedTotal: (0.85)*totalPrice },
+            { coupon: 50, discountedTotal: (0.5)*totalPrice },
+            { coupon: 100, discountedTotal: (0)*totalPrice },
+            { coupon: 0, discountedTotal: (1)*totalPrice },
+        ])('Should apply discount of $coupon% over total with coupon', ({coupon, discountedTotal}) => {
             const order = new Order('935.411.347-80');
             order.addItem(new Item(1, 'Books', 'Clean Architecture', 50), 1);
             order.addItem(new Item(2, 'Books', 'Clean Code', 70), 1);
             order.addItem(new Item(3, 'Food', 'Coffee Beans 1kg', 10), 4);
-            
-            const couponPercentage = 10
-            order.applyCoupon(new Coupon(1, 'happy-coupon', couponPercentage));
 
-            const price = order.getTotalPrice();
-            const total = order.getTotal();
-            
-            expect(total).toBe(price - (price*(couponPercentage/100)));
-        });
-        test('Should apply discount of 50% over total with coupon', () => {
-            const order = new Order('935.411.347-80');
-            order.addItem(new Item(1, 'Books', 'Clean Architecture', 50), 1);
-            order.addItem(new Item(2, 'Books', 'Clean Code', 70), 5);
-            order.addItem(new Item(3, 'Food', 'Coffee Beans 1kg', 10), 4);
-            order.addItem(new Item(3, 'Food', 'Coffee Beans 3kg', 10), 7);
-            
-            const couponPercentage = 50
+            const couponPercentage = coupon;
             order.applyCoupon(new Coupon(1, 'happy-coupon', couponPercentage));
-
-            const price = order.getTotalPrice();
-            const total = order.getTotal();
             
-            expect(total).toBe(price - (price*(couponPercentage/100)));
-        });
-        test('Should apply discount of 37.5% over total with coupon', () => {
-            const order = new Order('935.411.347-80');
-            order.addItem(new Item(1, 'Books', 'Clean Architecture', 50), 9);
-            order.addItem(new Item(2, 'Books', 'Clean Code', 70), 5);
-            order.addItem(new Item(3, 'Food', 'Coffee Beans 1kg', 10), 4);
-            order.addItem(new Item(3, 'Food', 'Coffee Beans 3kg', 10), 2);
-            order.addItem(new Item(3, 'Home Office', 'Table Plant', 2), 100);
-            
-            const couponPercentage = 37.5
-            order.applyCoupon(new Coupon(1, 'happy-coupon', couponPercentage));
-
-            const price = order.getTotalPrice();
-            const total = order.getTotal();
-            
-            expect(total).toBe(price - (price*(couponPercentage/100)));
+            expect(order.getTotal())
+                .toBe(discountedTotal);
         });
         test('Should discount 0 from order if no coupon applied', () => {
             const order = new Order('935.411.347-80');
@@ -100,13 +74,9 @@ describe('Creating Order', () => {
             order.addItem(new Item(3, 'Food', 'Coffee Beans 1kg', 10), 4);
             order.addItem(new Item(3, 'Food', 'Coffee Beans 3kg', 10), 2);
             order.addItem(new Item(3, 'Home Office', 'Table Plant', 2), 100);
-            
-            const couponPercentage = 37.5
 
-            const price = order.getTotalPrice();
-            const total = order.getTotal();
-            
-            expect(total).toBe(price);
+            expect(order.getTotal())
+                .toBe(order.getTotalPrice());
         });
     });
 });
