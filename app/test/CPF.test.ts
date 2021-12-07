8ed81d941899d885a7207acd1947c071ded9d46a
Hugo Gouveia <hgchrome@gmail.com>
2021-12-07 00:44:25 -0300

    refactor: Change access to CPF methods

--- app/test/CPF.test.ts
+++ app/test/CPF.test.ts
@@ -1,24 +1,28 @@
 import CPF from '../src/CPF';
 
 test.each([
-    { cpf: '', expected: false, reason: 'is an empty string' },
-    { cpf: '111.111.111-11', expected: false, reason: 'all digits are the same' },
-    { cpf: 'a935.411.347-80', expected: false, reason: 'has any letters' },
-    { cpf: '9)5.411.347-80', expected: false, reason: 'has any char that is not a digit' },
-    { cpf: '0111.111.111-11', expected: false, reason: 'has more than 11 digits' },
-    { cpf: '1111111111', expected: false, reason: 'has less than 11 digits' },
-    { cpf: '123.456.789-99', expected: false, reason: 'verifying digits are wrong' },
-    { cpf: '935.411.347-80', expected: true, reason: 'verifying digits are correct' },
+    { value: '', reason: 'is an empty string' },
+    { value: '111.111.111-11', reason: 'all digits are the same' },
+    { value: 'a935.411.347-80', reason: 'has any letters' },
+    { value: '9)5.411.347-80', reason: 'has any char that is not a digit' },
+    { value: '0111.111.111-11', reason: 'has more than 11 digits' },
+    { value: '1111111111', reason: 'has less than 11 digits' },
+    { value: '123.456.789-99', reason: 'verifying digits are wrong' },
+])('Should throw Error if $reason', ({value}) => {
+    expect(()=>new CPF(value)).toThrow(new Error('Invalid CPF'));
+});
+
+test.each([
+    { value: '935.411.347-80', reason: 'verifying digits are correct' },
     { 
-        cpf: '9   35   .4   11.347      -80', 
-        expected: true, 
+        value: '9   35   .4   11.347      -80',
         reason: 'verifying digits are correct even with spaces in string' 
     },
     { 
-        cpf: '93541134780', 
-        expected: true, 
+        value: '93541134780',
         reason: 'verifying digits are correct even with without formatting'
     },
-])('Should return $expected if $reason', ({cpf, expected}) => {
-    expect(CPF.isValid(cpf)).toBe(expected);
-});;
+])('Should create valid CPF if $reason', ({value}) => {
+    const cpf: CPF = new CPF(value);
+    expect(cpf).toBeDefined();
+});
--- app/test/Order.test.ts
+++ app/test/Order.test.ts
@@ -1,5 +1,5 @@
 import Coupon from '../src/Coupon';
-import Item from '../src/Item';
+import Product from '../src/Product';
 import Order from '../src/Order';
 
 describe('Creating Order', () => {
@@ -15,33 +15,33 @@ describe('Creating Order', () => {
     describe('Adding items', () => {
         test('Should create an Order with 1 item', () => {
             const order = new Order('935.411.347-80');
-            order.addItem(new Item(1, 'Books', 'Clean Architecture', 50), 1);
+            order.addItem(new Product(1, 'Books', 'Clean Architecture', 50), 1);
             const itemCount = order.getItemCount();
             expect(itemCount).toBe(1);
         });
 
         test('Should create an Order with 3 items', () => {
             const order = new Order('935.411.347-80');
-            order.addItem(new Item(1, 'Books', 'Clean Architecture', 50), 1);
-            order.addItem(new Item(2, 'Books', 'Clean Code', 70), 1);
-            order.addItem(new Item(3, 'Food', 'Coffee Beans 1kg', 10), 4);
+            order.addItem(new Product(1, 'Books', 'Clean Architecture', 50), 1);
+            order.addItem(new Product(2, 'Books', 'Clean Code', 70), 1);
+            order.addItem(new Product(3, 'Food', 'Coffee Beans 1kg', 10), 4);
             const itemCount = order.getItemCount();
             expect(itemCount).toBe(3);
         });
 
         test('Should return the order total', () => {
             const order = new Order('935.411.347-80');
-            order.addItem(new Item(1, 'Books', 'Clean Architecture', 50), 1);
-            order.addItem(new Item(2, 'Books', 'Clean Code', 70), 1);
+            order.addItem(new Product(1, 'Books', 'Clean Architecture', 50), 1);
+            order.addItem(new Product(2, 'Books', 'Clean Code', 70), 1);
             const total = order.getTotalPrice();
             expect(total).toBe((50 * 1 + 70 * 1));
         });
 
         test('Should return the order total with items having different quantities', () => {
             const order = new Order('935.411.347-80');
-            order.addItem(new Item(1, 'Books', 'Clean Architecture', 50), 1);
-            order.addItem(new Item(2, 'Books', 'Clean Code', 70), 1);
-            order.addItem(new Item(3, 'Food', 'Coffee Beans 1kg', 10), 4);
+            order.addItem(new Product(1, 'Books', 'Clean Architecture', 50), 1);
+            order.addItem(new Product(2, 'Books', 'Clean Code', 70), 1);
+            order.addItem(new Product(3, 'Food', 'Coffee Beans 1kg', 10), 4);
             const total = order.getTotalPrice();
             expect(total).toBe((50 * 1 + 70 * 1 + 10 * 4));
         });
@@ -57,9 +57,9 @@ describe('Creating Order', () => {
             { coupon: 0, discountedTotal: (1)*totalPrice },
         ])('Should apply discount of $coupon% over total with coupon', ({coupon, discountedTotal}) => {
             const order = new Order('935.411.347-80');
-            order.addItem(new Item(1, 'Books', 'Clean Architecture', 50), 1);
-            order.addItem(new Item(2, 'Books', 'Clean Code', 70), 1);
-            order.addItem(new Item(3, 'Food', 'Coffee Beans 1kg', 10), 4);
+            order.addItem(new Product(1, 'Books', 'Clean Architecture', 50), 1);
+            order.addItem(new Product(2, 'Books', 'Clean Code', 70), 1);
+            order.addItem(new Product(3, 'Food', 'Coffee Beans 1kg', 10), 4);
 
             const couponPercentage = coupon;
             order.applyCoupon(new Coupon(1, 'happy-coupon', couponPercentage));
@@ -69,11 +69,11 @@ describe('Creating Order', () => {
         });
         test('Should discount 0 from order if no coupon applied', () => {
             const order = new Order('935.411.347-80');
-            order.addItem(new Item(1, 'Books', 'Clean Architecture', 50), 9);
-            order.addItem(new Item(2, 'Books', 'Clean Code', 70), 5);
-            order.addItem(new Item(3, 'Food', 'Coffee Beans 1kg', 10), 4);
-            order.addItem(new Item(3, 'Food', 'Coffee Beans 3kg', 10), 2);
-            order.addItem(new Item(3, 'Home Office', 'Table Plant', 2), 100);
+            order.addItem(new Product(1, 'Books', 'Clean Architecture', 50), 9);
+            order.addItem(new Product(2, 'Books', 'Clean Code', 70), 5);
+            order.addItem(new Product(3, 'Food', 'Coffee Beans 1kg', 10), 4);
+            order.addItem(new Product(3, 'Food', 'Coffee Beans 3kg', 10), 2);
+            order.addItem(new Product(3, 'Home Office', 'Table Plant', 2), 100);
 
             expect(order.getTotal())
                 .toBe(order.getTotalPrice());
